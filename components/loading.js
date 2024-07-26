fb658eec1002790b9edcc205097b383e9ff37155
Daniel Ramsgard <Daniel.Ramsgard@unc.edu>
2024-07-26 10:18:46 -0400

    timer

--- components/loading.js
+++ components/loading.js
@@ -1,22 +1,60 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { View, StyleSheet } from "react-native";
 import Number from "./number";
 import Colon from "./colon";
 
 const Loading = () => {
+    const [timer, setTimer] = useState({
+        days: 0,
+        hours: 0,
+        minutes: 0,
+        seconds: 0,
+    });
+
+    const targetDate = new Date("2024-07-31T12:00:00"); // Set your target date here
+
+    useEffect(() => {
+        const calculateTimeLeft = () => {
+            const now = new Date();
+            const difference = targetDate - now;
+
+            let timeLeft = {
+                days: 0,
+                hours: 0,
+                minutes: 0,
+                seconds: 0,
+            };
+
+            if (difference > 0) {
+                timeLeft = {
+                    days: Math.floor(difference / (1000 * 60 * 60 * 24)),
+                    hours: Math.floor((difference % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)),
+                    minutes: Math.floor((difference % (1000 * 60 * 60)) / (1000 * 60)),
+                    seconds: Math.floor((difference % (1000 * 60)) / 1000),
+                };
+            }
+
+            setTimer(timeLeft);
+        };
+
+        const interval = setInterval(calculateTimeLeft, 1000);
+
+        return () => clearInterval(interval);
+    }, []);
+
     return (
         <>
             <View style={styles.container}>
                 <View style={styles.timerContainer}>
-                    <Number time={31} label={"DAYS"}/>
+                    <Number time={timer.days} label={"DAYS"} />
                     <Colon />
-                    <Number time={31} label={"HOURS"}/>
+                    <Number time={timer.hours} label={"HOURS"} />
                     <Colon />
-                    <Number time={31} label={"MINUTES"}/>
+                    <Number time={timer.minutes} label={"MINUTES"} />
                     <Colon />
-                    <Number time={31} label={"SECONDS"}/>
+                    <Number time={timer.seconds} label={"SECONDS"} />
                 </View>
-            </View> 
+            </View>
         </>
     );
 }
--- components/number.js
+++ components/number.js
@@ -31,6 +31,7 @@ const styles = StyleSheet.create({
     },
     top: {
         height: 65,
+        width: 75,
         justifyContent: "center",
         alignItems: "center",
     },
