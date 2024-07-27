19c62b03d9713268ccc97f9c8574c35e1c3b5d3c
Daniel Ramsgard <Daniel.Ramsgard@unc.edu>
2024-07-27 10:14:32 -0400

    responsive

--- components/pdfStylus.js
+++ components/pdfStylus.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { View, Text, StyleSheet, SafeAreaView } from "react-native";
+import { View, Text, StyleSheet, SafeAreaView, Dimensions } from "react-native";
 import LogoFill from "./logoFill";
 import Eraser from "./eraser";
 import Stylus from "./stylus";
@@ -7,8 +7,11 @@ import Chevron from "./chevron";
 import ZoomIn from "./zoomIn";
 import ZoomOut from "./zoomOut";
 import Undo from "./undo";
+import Forward from "./forward";
 
 const PDFStylus = () => {
+    const windowWidth = Dimensions.get('window').width;
+
     return (
         <>
             <SafeAreaView>
@@ -20,11 +23,11 @@ const PDFStylus = () => {
                 </View>
                 <View style={styles.toolbar}>
                     <View style={styles.zoomContainer}>  
-                        <View style={[styles.container]}>
+                        <View style={styles.container}>
                             <Undo />
                         </View>
-                        <View style={[styles.container, { transform: [{rotate: "180deg"}] }]}>
-                            <Undo />
+                        <View style={styles.container}>
+                            <Forward />
                         </View>
                         <View style={styles.container}>
                             <ZoomIn />
@@ -33,7 +36,7 @@ const PDFStylus = () => {
                             <ZoomOut />
                         </View>
                     </View>
-                    <View style={styles.barContainer}>
+                    <View style={[styles.barContainer, { width: windowWidth < 667 ? "5%" : "10%" }]}>
                         <View style={styles.bar}>
 
                         </View>
@@ -56,7 +59,7 @@ const PDFStylus = () => {
                             </View>
                         </View>
                     </View>
-                    <View style={styles.barContainer}>
+                    <View style={[styles.barContainer, { width: windowWidth < 667 ? "5%" : "10%" }]}>
                         <View style={styles.bar}>
 
                         </View>
@@ -87,13 +90,13 @@ const PDFStylus = () => {
 const styles = StyleSheet.create({
     bar: {
         width: 1,
-        height: "100%",
+        height: "65%",
         backgroundColor: "rgb(230,230,230)",
     },
     barContainer: {
         justifyContent: "center",
         alignItems: "center",
-        width: 60,
+        width: "10%",
         height: "100%"
     },
     zoomContainer: {
--- components/undo.js
+++ components/undo.js
@@ -2,17 +2,20 @@ import React from "react";
 import Svg, { Path } from "react-native-svg";
 
 const Undo = (props) => (
-    <Svg
-        xmlns="http://www.w3.org/2000/svg"
-        width={14}
-        height={14}
-        fill="currentColor"
-        viewBox="0 0 16 16"
-        {...props}
-    >
-        <Path d="M8.021 11.9 3.453 8.62a.72.72 0 0 1 0-1.238L8.021 4.1a.716.716 0 0 1 1.079.619V6c1.5 0 6 0 7 8-2.5-4.5-7-4-7-4v1.281c0 .56-.606.898-1.079.62z" />
-        <Path d="M5.232 4.293a.5.5 0 0 1-.106.7L1.114 7.945l-.042.028a.147.147 0 0 0 0 .252l.042.028 4.012 2.954a.5.5 0 1 1-.593.805L.539 9.073a1.147 1.147 0 0 1 0-1.946l3.994-2.94a.5.5 0 0 1 .699.106" />
-    </Svg>
+  <Svg
+    xmlns="http://www.w3.org/2000/svg"
+    width={14}
+    height={14}
+    fill="currentColor"
+    viewBox="0 0 16 16"
+    {...props}
+  >
+    <Path
+      fillRule="evenodd"
+      d="M8 3a5 5 0 1 1-4.546 2.914.5.5 0 0 0-.908-.417A6 6 0 1 0 8 2z"
+    />
+    <Path d="M8 4.466V.534a.25.25 0 0 0-.41-.192L5.23 2.308a.25.25 0 0 0 0 .384l2.36 1.966A.25.25 0 0 0 8 4.466" />
+  </Svg>
 );
 
 export default Undo;
